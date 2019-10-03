1f332811bc416e1ff6b59c82adb25f810d690163
Erik Roberts <erik@bugg.co>
2019-10-03 19:52:19 -0400

    added delete dorm logic

--- server/api/dorms/dorms.controller.js
+++ server/api/dorms/dorms.controller.js
@@ -2,8 +2,19 @@ const logger = require('../../modules/logger')
 
 const Dorm = require('./dorms.model')
 
-function deleteDorm (ctx) {
+async function deleteDorm (ctx) {
+  if (!ctx.params || !ctx.params.id) {
+    return ctx.badRequest('You must provide a dorm ID to delete.')
+  }
 
+  const d = await Dorm.findOne({ _id: ctx.params.id })
+  if (!d) {
+    return ctx.notFound('Dorm does not exist')
+  }
+
+  d.remove()
+  logger.info('Deleted dorm ID ' + d._id + ' as requested by user ' + ctx.state.user.rcs_id)
+  ctx.noContent()
 }
 
 function createDorm (ctx) {
